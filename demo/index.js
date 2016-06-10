0c4217428bd0aa271eff27d17211263c7a5946b8
Fernando Daciuk <f.daciuk@gmail.com>
2016-06-10 13:32:32 -0300

    Improve dumb component

--- demo/index.js
+++ demo/index.js
@@ -2,12 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PageSpeedScore from './../src/index';
 
-class Demo extends React.Component {
-    render() {
-      return (
-        <PageSpeedScore url="http://cauealves.com/" />
-      );
-    }
-};
+const Demo = () => (
+  <PageSpeedScore url="http://cauealves.com/" />
+);
 
 ReactDOM.render(<Demo />, document.getElementById('demo'));
